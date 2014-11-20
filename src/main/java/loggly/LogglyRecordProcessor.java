84a2ccef676f24c858fcede2c6823ed60dd3d087
d-smith <doug.smith.mail@gmail.com>
2014-11-20 11:32:41 -0700

    Refactored LogglyRecordProcessor to subclass LogStreamProcessor

--- src/main/java/loggly/LogglyRecordProcessor.java
+++ src/main/java/loggly/LogglyRecordProcessor.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorC
 import com.amazonaws.services.kinesis.clientlibrary.types.ShutdownReason;
 import com.amazonaws.services.kinesis.model.Record;
 import loggly.*;
+import logreader.LogStreamProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import retrofit.RestAdapter;
@@ -17,7 +18,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.util.List;
 
-public class LogglyRecordProcessor implements IRecordProcessor {
+public class LogglyRecordProcessor extends LogStreamProcessor {
     private final Logger LOG = LoggerFactory.getLogger(LogglyRecordProcessor.class);
     private String kinesisShardId;
 
@@ -49,69 +50,6 @@ public class LogglyRecordProcessor implements IRecordProcessor {
     }
 
 
-    @Override
-    public void initialize(String shardId) {
-
-        LOG.info("Initializing record processor for shard: " + shardId);
-        this.kinesisShardId = shardId;
-    }
-
-    @Override
-    public void processRecords(List<Record> records, IRecordProcessorCheckpointer checkpointer) {
-        LOG.info("Processing " + records.size() + " records from " + kinesisShardId);
-
-        // Process records and perform all exception handling.
-        processRecordsWithRetries(records);
-
-        // Checkpoint once every checkpoint interval.
-        if (System.currentTimeMillis() > nextCheckpointTimeInMillis) {
-            checkpoint(checkpointer);
-            nextCheckpointTimeInMillis = System.currentTimeMillis() + CHECKPOINT_INTERVAL_MILLIS;
-        }
-    }
-
-    @Override
-    public void shutdown(IRecordProcessorCheckpointer checkpointer, ShutdownReason shutdownReason) {
-
-        LOG.info("Shutting down record processor for shard: " + kinesisShardId);
-        // Important to checkpoint after reaching end of shard, so we can start processing data from child shards.
-        if (shutdownReason == ShutdownReason.TERMINATE) {
-            checkpoint(checkpointer);
-        }
-    }
-
-    private void checkpoint(IRecordProcessorCheckpointer checkpointer) {
-        LOG.info("Checkpointing shard " + kinesisShardId);
-        for (int i = 0; i < NUM_RETRIES; i++) {
-            try {
-                checkpointer.checkpoint();
-                break;
-            } catch (ShutdownException se) {
-                // Ignore checkpoint if the processor instance has been shutdown (fail over).
-                LOG.info("Caught shutdown exception, skipping checkpoint.", se);
-                break;
-            } catch (ThrottlingException e) {
-                // Backoff and re-attempt checkpoint upon transient failures
-                if (i >= (NUM_RETRIES - 1)) {
-                    LOG.error("Checkpoint failed after " + (i + 1) + "attempts.", e);
-                    break;
-                } else {
-                    LOG.info("Transient issue when checkpointing - attempt " + (i + 1) + " of "
-                            + NUM_RETRIES, e);
-                }
-            } catch (InvalidStateException e) {
-                // This indicates an issue with the DynamoDB table (check for table, provisioned IOPS).
-                LOG.error("Cannot save checkpoint to the DynamoDB table used by the Amazon Kinesis Client Library.", e);
-                break;
-            }
-            try {
-                Thread.sleep(BACKOFF_TIME_IN_MILLIS);
-            } catch (InterruptedException e) {
-                LOG.debug("Interrupted sleep", e);
-            }
-        }
-    }
-
     private LogMessage msgFromData(String raw) {
         if(CallTimeMsg.isTimeMessage(raw)) {
             return new CallTimeMsg(raw);
@@ -121,7 +59,8 @@ public class LogglyRecordProcessor implements IRecordProcessor {
     }
 
 
-    private void processRecordsWithRetries(List<Record> records) {
+    @Override
+    protected void processRecordsWithRetries(List<Record> records) {
         for (Record record : records) {
             boolean processedSuccessfully = false;
             String data = null;
