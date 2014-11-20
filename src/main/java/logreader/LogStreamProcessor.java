20495274cb035b097c7c2e9e98c0278c26cc3d51
d-smith <doug.smith.mail@gmail.com>
2014-11-20 10:51:23 -0700

    Started refactoring into base class/subclass hierarchy

---
+++ src/main/java/logreader/LogStreamProcessor.java
@@ -0,0 +1,89 @@
+package logreader;
+
+
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.InvalidStateException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.ShutdownException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.ThrottlingException;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessor;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorCheckpointer;
+import com.amazonaws.services.kinesis.clientlibrary.types.ShutdownReason;
+import com.amazonaws.services.kinesis.model.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public abstract class LogStreamProcessor implements IRecordProcessor {
+
+    private final Logger LOG = LoggerFactory.getLogger(LogStreamProcessor.class);
+    private String kinesisShardId;
+
+    protected static final long BACKOFF_TIME_IN_MILLIS = 3000L;
+    protected static final int NUM_RETRIES = 10;
+
+    private static final long CHECKPOINT_INTERVAL_MILLIS = 60000L;
+    private long nextCheckpointTimeInMillis;
+
+    @Override
+    public void initialize(String shardId) {
+        LOG.info("Initializing record processor for shard: " + shardId);
+        this.kinesisShardId = shardId;
+    }
+
+    @Override
+    public void processRecords(List<Record> records, IRecordProcessorCheckpointer checkpointer) {
+        LOG.info("Processing " + records.size() + " records from " + kinesisShardId);
+
+        // Process records and perform all exception handling.
+        processRecordsWithRetries(records);
+
+        // Checkpoint once every checkpoint interval.
+        if (System.currentTimeMillis() > nextCheckpointTimeInMillis) {
+            checkpoint(checkpointer);
+            nextCheckpointTimeInMillis = System.currentTimeMillis() + CHECKPOINT_INTERVAL_MILLIS;
+        }
+    }
+
+    @Override
+    public void shutdown(IRecordProcessorCheckpointer checkpointer, ShutdownReason shutdownReason) {
+        LOG.info("Shutting down record processor for shard: " + kinesisShardId);
+        // Important to checkpoint after reaching end of shard, so we can start processing data from child shards.
+        if (shutdownReason == ShutdownReason.TERMINATE) {
+            checkpoint(checkpointer);
+        }
+    }
+
+    private void checkpoint(IRecordProcessorCheckpointer checkpointer) {
+        LOG.info("Checkpointing shard " + kinesisShardId);
+        for (int i = 0; i < NUM_RETRIES; i++) {
+            try {
+                checkpointer.checkpoint();
+                break;
+            } catch (ShutdownException se) {
+                // Ignore checkpoint if the processor instance has been shutdown (fail over).
+                LOG.info("Caught shutdown exception, skipping checkpoint.", se);
+                break;
+            } catch (ThrottlingException e) {
+                // Backoff and re-attempt checkpoint upon transient failures
+                if (i >= (NUM_RETRIES - 1)) {
+                    LOG.error("Checkpoint failed after " + (i + 1) + "attempts.", e);
+                    break;
+                } else {
+                    LOG.info("Transient issue when checkpointing - attempt " + (i + 1) + " of "
+                            + NUM_RETRIES, e);
+                }
+            } catch (InvalidStateException e) {
+                // This indicates an issue with the DynamoDB table (check for table, provisioned IOPS).
+                LOG.error("Cannot save checkpoint to the DynamoDB table used by the Amazon Kinesis Client Library.", e);
+                break;
+            }
+            try {
+                Thread.sleep(BACKOFF_TIME_IN_MILLIS);
+            } catch (InterruptedException e) {
+                LOG.debug("Interrupted sleep", e);
+            }
+        }
+    }
+
+    protected abstract void processRecordsWithRetries(List<Record> records);
+}
--- src/main/java/logstash/LogstashRecordProcessor.java
+++ src/main/java/logstash/LogstashRecordProcessor.java
@@ -11,6 +11,7 @@ import com.google.gson.Gson;
 import loggly.CallTimeMsg;
 import loggly.LogMessage;
 import loggly.RawMessage;
+import logreader.LogStreamProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +22,7 @@ import java.nio.charset.CharsetDecoder;
 import java.util.List;
 
 
-public class LogstashRecordProcessor implements IRecordProcessor {
+public class LogstashRecordProcessor extends LogStreamProcessor {
 
     String logstashHost;
     int logstashPort;
@@ -50,78 +51,8 @@ public class LogstashRecordProcessor implements IRecordProcessor {
 
     private final CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
 
-    // Backoff and retry settings
-    private static final long BACKOFF_TIME_IN_MILLIS = 3000L;
-    private static final int NUM_RETRIES = 10;
 
     private final Logger LOG = LoggerFactory.getLogger(LogstashRecordProcessor.class);
-    private String kinesisShardId;
-
-    // Checkpoint about once a minute
-    private static final long CHECKPOINT_INTERVAL_MILLIS = 60000L;
-    private long nextCheckpointTimeInMillis;
-
-    @Override
-    public void initialize(String shardId) {
-        LOG.info("Initializing record processor for shard: " + shardId);
-        this.kinesisShardId = shardId;
-    }
-
-    @Override
-    public void processRecords(List<Record> records, IRecordProcessorCheckpointer checkpointer) {
-        LOG.info("Processing " + records.size() + " records from " + kinesisShardId);
-
-        // Process records and perform all exception handling.
-        processRecordsWithRetries(records);
-
-        // Checkpoint once every checkpoint interval.
-        if (System.currentTimeMillis() > nextCheckpointTimeInMillis) {
-            checkpoint(checkpointer);
-            nextCheckpointTimeInMillis = System.currentTimeMillis() + CHECKPOINT_INTERVAL_MILLIS;
-        }
-    }
-
-    @Override
-    public void shutdown(IRecordProcessorCheckpointer checkpointer, ShutdownReason shutdownReason) {
-        LOG.info("Shutting down record processor for shard: " + kinesisShardId);
-        // Important to checkpoint after reaching end of shard, so we can start processing data from child shards.
-        if (shutdownReason == ShutdownReason.TERMINATE) {
-            checkpoint(checkpointer);
-        }
-    }
-
-
-    private void checkpoint(IRecordProcessorCheckpointer checkpointer) {
-        LOG.info("Checkpointing shard " + kinesisShardId);
-        for (int i = 0; i < NUM_RETRIES; i++) {
-            try {
-                checkpointer.checkpoint();
-                break;
-            } catch (ShutdownException se) {
-                // Ignore checkpoint if the processor instance has been shutdown (fail over).
-                LOG.info("Caught shutdown exception, skipping checkpoint.", se);
-                break;
-            } catch (ThrottlingException e) {
-                // Backoff and re-attempt checkpoint upon transient failures
-                if (i >= (NUM_RETRIES - 1)) {
-                    LOG.error("Checkpoint failed after " + (i + 1) + "attempts.", e);
-                    break;
-                } else {
-                    LOG.info("Transient issue when checkpointing - attempt " + (i + 1) + " of "
-                            + NUM_RETRIES, e);
-                }
-            } catch (InvalidStateException e) {
-                // This indicates an issue with the DynamoDB table (check for table, provisioned IOPS).
-                LOG.error("Cannot save checkpoint to the DynamoDB table used by the Amazon Kinesis Client Library.", e);
-                break;
-            }
-            try {
-                Thread.sleep(BACKOFF_TIME_IN_MILLIS);
-            } catch (InterruptedException e) {
-                LOG.debug("Interrupted sleep", e);
-            }
-        }
-    }
 
     private LogMessage msgFromData(String raw) {
         if(CallTimeMsg.isTimeMessage(raw)) {
@@ -131,8 +62,8 @@ public class LogstashRecordProcessor implements IRecordProcessor {
         }
     }
 
-
-    private void processRecordsWithRetries(List<Record> records) {
+    @Override
+    protected void processRecordsWithRetries(List<Record> records) {
         for (Record record : records) {
             boolean processedSuccessfully = false;
             String data = null;
